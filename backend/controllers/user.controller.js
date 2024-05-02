87e63f3d12f0cc8902cade9162457a2021695313
shekhar bansal <bansalshekhar2002@gmail.com>
2024-05-02 10:44:37 +0530

    add routes to update and get user status

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import { setUserStatus , getUserStatus } from "./auth.controller.js";
 
 export const getUsersForSidebar = async (req, res) => {
 	try {
@@ -10,5 +11,32 @@ export const getUsersForSidebar = async (req, res) => {
 	} catch (error) {
 		console.error("Error in getUsersForSidebar: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+//update user status
+export const updateStatus = async (req, res) => {
+	const { userId } = req.params;
+	const { status } = req.body;
+
+	try {
+			const updateResult = await setUserStatus(userId, status);
+			res.status(200).json(updateResult);
+	} catch (error) {
+			console.error("Error updating user status: ", error.message);
+			res.status(500).json({ success: false, message: "Internal server error" });
+	}
+};
+
+// get user status
+export const getStatus = async (req, res) => {
+	const { userId } = req.params;
+
+	try {
+			const status = await getUserStatus(userId);
+			res.status(200).json({ status });
+	} catch (error) {
+			console.error("Error getting user status: ", error.message);
+			res.status(500).json({ success: false, message: "Internal server error" });
 	}
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUsersForSidebar } from "../controllers/user.controller.js";
+import { getUsersForSidebar , updateStatus, getStatus } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getUsersForSidebar);
+
+router.get("/:id", protectRoute, getStatus);
+
+router.post("/status/:id", protectRoute, updateStatus);
 
 export default router;
