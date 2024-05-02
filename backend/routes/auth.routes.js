febd331434a0921d9665b8cd11b1e9c0c29186fe
shekhar bansal <bansalshekhar2002@gmail.com>
2024-05-02 08:49:30 +0530

    routes have been created

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { login, logout, signup } from "../controllers/auth.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
@@ -7,6 +8,6 @@ router.post("/signup", signup);
 
 router.post("/login", login);
 
-router.post("/logout", logout);
+router.post("/logout", protectRoute, logout);
 
 export default router;
