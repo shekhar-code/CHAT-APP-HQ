56bbd6897a9e427737559d3703a192b0b03cb1bc
shekhar bansal <bansalshekhar2002@gmail.com>
2024-05-02 08:54:28 +0530

    set and get messages and handles LLM response

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,12 +1,20 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-// import { getReceiverSocketId, io } from "../socket/socket.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
+import {getUserStatus, generateResponse} from "./auth.controller.js"
 
 export const sendMessage = async (req, res) => {
 	try {
 		const { message } = req.body;
 		const { id: receiverId } = req.params;
 		const senderId = req.user._id;
+
+		// Check receiver's status
+		const receiverStatus = await getUserStatus(receiverId);
+
+		let responseMessage = message;
+		// let newsenderId = senderId;
+		let newreceiverId = receiverId;
 
 		let conversation = await Conversation.findOne({
 			participants: { $all: [senderId, receiverId] },
@@ -18,10 +26,40 @@ export const sendMessage = async (req, res) => {
 			});
 		}
 
+		// console.log(message);
+
+
+// generateResponse({"inputs": "Can you please let us know more details about your "}).then((response) => {
+// 	console.log(JSON.stringify(response));
+// });
+
+		if (receiverStatus === 'BUSY') {
+
+
+
+			const response = await generateResponse({ inputs: message });
+
+			// Handle the response
+			const data = response; // No need to stringify again, as response is already an object
+			if (Array.isArray(data)) {
+					// Handle array data
+					const generatedMessage = data[0]['generated_text'];
+					responseMessage = generatedMessage.split('\n')[0];
+			} else {
+					// Handle string data
+					responseMessage = data;
+			}
+
+      console.log(responseMessage);
+
+			newreceiverId = senderId;
+
+		}
+
 		const newMessage = new Message({
 			senderId,
-			receiverId,
-			message,
+			receiverId: newreceiverId,
+			message: responseMessage,
 		});
 
 		if (newMessage) {
@@ -35,16 +73,16 @@ export const sendMessage = async (req, res) => {
 		await Promise.all([conversation.save(), newMessage.save()]);
 
 		// SOCKET IO FUNCTIONALITY WILL GO HERE
-		// const receiverSocketId = getReceiverSocketId(receiverId);
-		// if (receiverSocketId) {
-		// 	// io.to(<socket_id>).emit() used to send events to specific client
-		// 	io.to(receiverSocketId).emit("newMessage", newMessage);
-		// }
+		const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
 
 		res.status(201).json(newMessage);
 	} catch (error) {
 		console.log("Error in sendMessage controller: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
+		res.status(500).json({ error: "Internal server error 1" });
 	}
 };
 
